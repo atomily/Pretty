88f5da3a1d9ba2dc05d9664674d743b8d7a199c2
Michael Golebiewski <promichael01@gmail.com>
2016-10-05 23:10:23 -0700

    Version 1

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+// test
+?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Home - Pretty</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/normalize.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/theme.css?<?php echo time(); ?>">
+  <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+ </head>
+ <body>
+  <div class="navigation-container">
+   <div class="navigation">
+    <div class="logo-container">
+     <a href="#" class="logo pulse">Pretty</a>
+    </div>
+    <ul>
+     <li><a href="#">Home</a></li>
+     <li><a href="#">Docs</a></li>
+     <li><a href="#">Contact</a></li>
+    </ul>
+   </div>
+  </div>
+  <div class="content-container">
+   <div class="content">
+    <h1 class="title pulse">Hello World!</h1>
+    <h2 class="subtitle">We are a team of designers & developers that are ready to create your dream site.</h2>
+    <div class="btn-g">
+     <a href="#" class="btn-p">Follow Us On Github</a>
+    </div>
+   </div>
+  </div>
+  <div class="footer-container">
+   <div class="footer">
+    <div class="social">
+     <a href="#"><i class="fa fa-github pulse" aria-hidden="true"></i></a>
+     <a href="#"><i class="fa fa-linkedin-square pulse" aria-hidden="true"></i></a>
+     <a href="#"><i class="fa fa-twitter pulse" aria-hidden="true"></i></a>
+     <a href="#"><i class="fa fa-paper-plane pulse" aria-hidden="true"></i></a>
+    </div>
+    <div class="copy">
+     <p><i class="fa fa-copyright" aria-hidden="true"></i> <?php echo date("Y"); ?> <a href="#" class="pulse">TBD</a> All Rights Reserved</p>
+    </div>
+   </div>
+  </div>
+  <!-- js -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.min.js"></script>
+  <!--[if lt IE 9]>
+   <script src="assets/js/html5shiv.js"></script>
+  <![endif]-->
+  <script type="text/javascript">
+    // test
+  </script>
+ </body>
+</html>
