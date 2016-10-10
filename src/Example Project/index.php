023d87d8c782288b076ffd15a21afebc93467a14
Michael Golebiewski <promichael01@gmail.com>
2016-10-09 22:14:59 -0700

    Version 1.0.0 Upload 1/5
    First theme uploaded. This contains everything in 1. Other versions will
    be available tomorrow.

--- src/Example Project/index.php
+++ src/Example Project/index.php
@@ -1,6 +1,3 @@
-<?php
-// test
-?>
 <!DOCTYPE html>
 <html>
  <head>
@@ -9,7 +6,6 @@
   <title>Home - Pretty</title>
   <link rel="stylesheet" type="text/css" href="assets/css/normalize.css">
   <link rel="stylesheet" type="text/css" href="assets/css/theme.css?<?php echo time(); ?>">
-  <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
   <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
  </head>
  <body>
@@ -19,16 +15,15 @@
      <a href="#" class="logo pulse">Pretty</a>
     </div>
     <ul>
-     <li><a href="#">Home</a></li>
-     <li><a href="#">Docs</a></li>
-     <li><a href="#">Contact</a></li>
+     <li><a href="index.php">Home</a></li>
+     <li><a href="portfolio.php">Portfolio</a></li>
     </ul>
    </div>
   </div>
   <div class="content-container">
    <div class="content">
     <h1 class="title pulse">Hello World!</h1>
-    <h2 class="subtitle">We are a team of designers & developers that are ready to create your dream site.</h2>
+    <h2 class="subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla turpis dolor. Aliquam non imperdiet est. Nullam eu eros ac nulla aliquet lacinia et nec elit. </h2>
     <div class="btn-g">
      <a href="#" class="btn-p">Follow Us On Github</a>
     </div>
@@ -43,18 +38,14 @@
      <a href="#"><i class="fa fa-paper-plane pulse" aria-hidden="true"></i></a>
     </div>
     <div class="copy">
-     <p><i class="fa fa-copyright" aria-hidden="true"></i> <?php echo date("Y"); ?> <a href="#" class="pulse">TBD</a> All Rights Reserved</p>
+     <p><i class="fa fa-copyright" aria-hidden="true"></i> <?php echo date("Y"); ?> <a href="#" class="pulse">Pretty</a> All Rights Reserved</p>
     </div>
    </div>
   </div>
   <!-- js -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.min.js"></script>
   <!--[if lt IE 9]>
    <script src="assets/js/html5shiv.js"></script>
   <![endif]-->
-  <script type="text/javascript">
-    // test
-  </script>
  </body>
 </html>
---
+++ src/Example Project/portfolio.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Portfolio - Pretty</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/normalize.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/theme.css?<?php echo time(); ?>">
+  <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+ </head>
+ <body>
+  <div class="navigation-container">
+   <div class="navigation">
+    <div class="logo-container">
+     <a href="#" class="logo pulse">Pretty</a>
+    </div>
+    <ul>
+     <li><a href="index.php">Home</a></li>
+     <li><a href="portfolio.php">Portfolio</a></li>
+    </ul>
+   </div>
+  </div>
+  <div class="content-container">
+   <div class="content">
+    <h1 class="title pulse">Selected Works from 2016 - <?php echo date("Y");?></h1>
+    <div class="filter">
+     <p class="pu">Table Filter</p>
+     <div class="form-p">
+      <a href="#">Lorem Ipsum <span class="badge">21</span></a>
+      <a href="#">Lorem Ipsum <span class="badge">488</span></a>
+      <a href="#">Lorem Ipsum <span class="badge">76</span></a>
+      <a href="#">Lorem Ipsum <span class="badge">9</span></a>
+      <a href="#">Lorem Ipsum <span class="badge">7</span></a>
+      <a href="#">Lorem Ipsum <span class="badge">82</span></a>
+     </div>
+    </div>
+    <div class="table">
+     <h3>2 Years of Designing and Developing</h3>
+     <h2>3 + projects and counting!</h2>
+     <h1>3 Results</h1>
+     <div class="data">
+      <h1 class="dTitle p">2016</h1>
+      <div class="item">
+       <h1 class="iTitle">Lorem Ipsum</h1>
+       <div class="pRef">
+        <a href="#" data-tooltip="View Concept" data-placement="bottom"><i class="fa fa-paint-brush pulse"></i></a>
+        <a href="#" data-tooltip="View Website" data-placement="bottom"><i class="fa fa-picture-o pulse"></i></a>
+       </div>
+       <p class="iSub"><span class="pTag">Website</span> for Onyx</p>
+       <p class="desc">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </p>
+      </div>
+      <div class="item">
+       <h1 class="iTitle">Lorem Ipsum</h1>
+       <div class="pRef">
+        <a href="#" data-tooltip="View Concept" data-placement="bottom"><i class="fa fa-paint-brush pulse"></i></a>
+        <a href="#" data-tooltip="View Website" data-placement="bottom"><i class="fa fa-picture-o pulse"></i></a>
+       </div>
+       <p class="iSub"><span class="pTag">Website</span> for Onyx</p>
+       <p class="desc">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </p>
+      </div>
+     </div>
+     <div class="data">
+      <h1 class="dTitle p">2016</h1>
+      <div class="item">
+       <h1 class="iTitle">Lorem Ipsum</h1>
+       <div class="pRef">
+        <a href="#" data-tooltip="View Concept" data-placement="bottom"><i class="fa fa-paint-brush pulse"></i></a>
+        <a href="#" data-tooltip="View Website" data-placement="bottom"><i class="fa fa-picture-o pulse"></i></a>
+       </div>
+       <p class="iSub"><span class="pTag">Website</span> for Onyx</p>
+       <p class="desc">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </p>
+      </div>
+      <div class="item">
+       <h1 class="iTitle">Lorem Ipsum</h1>
+       <div class="pRef">
+        <a href="#" data-tooltip="View Concept" data-placement="bottom"><i class="fa fa-paint-brush pulse"></i></a>
+        <a href="#" data-tooltip="View Website" data-placement="bottom"><i class="fa fa-picture-o pulse"></i></a>
+       </div>
+       <p class="iSub"><span class="pTag">Website</span> for Onyx</p>
+       <p class="desc">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </p>
+      </div>
+     </div>
+     <div class="data">
+      <h1 class="dTitle p">2016</h1>
+      <div class="item">
+       <h1 class="iTitle">Lorem Ipsum</h1>
+       <div class="pRef">
+        <a href="#" data-tooltip="View Concept" data-placement="bottom"><i class="fa fa-paint-brush pulse"></i></a>
+        <a href="#" data-tooltip="View Website" data-placement="bottom"><i class="fa fa-picture-o pulse"></i></a>
+       </div>
+       <p class="iSub"><span class="pTag">Website</span> for Onyx</p>
+       <p class="desc">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </p>
+      </div>
+      <div class="item last">
+       <h1 class="iTitle">Lorem Ipsum</h1>
+       <div class="pRef">
+        <a href="#" data-tooltip="View Concept" data-placement="bottom"><i class="fa fa-paint-brush pulse"></i></a>
+        <a href="#" data-tooltip="View Website" data-placement="bottom"><i class="fa fa-picture-o pulse"></i></a>
+       </div>
+       <p class="iSub"><span class="pTag">Website</span> for Onyx</p>
+       <p class="desc">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </p>
+      </div>
+     </div>
+    </div>
+   </div>
+  </div>
+  <div class="footer-container">
+   <div class="footer">
+    <div class="social">
+     <a href="#"><i class="fa fa-github pulse" aria-hidden="true"></i></a>
+     <a href="#"><i class="fa fa-linkedin-square pulse" aria-hidden="true"></i></a>
+     <a href="#"><i class="fa fa-twitter pulse" aria-hidden="true"></i></a>
+     <a href="#"><i class="fa fa-paper-plane pulse" aria-hidden="true"></i></a>
+    </div>
+    <div class="copy">
+     <p><i class="fa fa-copyright" aria-hidden="true"></i> <?php echo date("Y"); ?> <a href="#" class="pulse">Pretty</a> All Rights Reserved</p>
+    </div>
+   </div>
+  </div>
+  <!-- js -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <!--[if lt IE 9]>
+   <script src="assets/js/html5shiv.js"></script>
+  <![endif]-->
+ </body>
+</html>
