8782c619bebfea45b55ad8bb36db0224f8ccc809
Michael Golebiewski <promichael01@gmail.com>
2016-10-09 22:34:44 -0700

    Version 1.0.0 Upload 2 / 5
    Added the hyper theme. Optimized the theme.scss | theme.css & removed
    useless files.

---
+++ src/hyper/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Home - Pretty</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/normalize.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/theme.css?<?php echo time(); ?>">
+  <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+ </head>
+ <body>
+  <div class="navigation-container">
+   <div class="navigation">
+    <div class="logo-container">
+     <a href="#" class="logo hyper">Pretty</a>
+    </div>
+    <ul>
+     <li><a href="index.php">Home</a></li>
+     <li><a href="portfolio.php">Portfolio</a></li>
+    </ul>
+   </div>
+  </div>
+  <div class="content-container">
+   <div class="content">
+    <h1 class="title hyper">Hello World!</h1>
+    <h2 class="subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla turpis dolor. Aliquam non imperdiet est. Nullam eu eros ac nulla aliquet lacinia et nec elit. </h2>
+    <div class="btn-g">
+     <a href="#" class="btn-h">Follow Us On Github</a>
+    </div>
+   </div>
+  </div>
+  <div class="footer-container">
+   <div class="footer">
+    <div class="social">
+     <a href="#"><i class="fa fa-github hyper" aria-hidden="true"></i></a>
+     <a href="#"><i class="fa fa-linkedin-square hyper" aria-hidden="true"></i></a>
+     <a href="#"><i class="fa fa-twitter hyper" aria-hidden="true"></i></a>
+     <a href="#"><i class="fa fa-paper-plane hyper" aria-hidden="true"></i></a>
+    </div>
+    <div class="copy">
+     <p><i class="fa fa-copyright" aria-hidden="true"></i> <?php echo date("Y"); ?> <a href="#" class="hyper">Pretty</a> All Rights Reserved</p>
+    </div>
+   </div>
+  </div>
+  <!-- js -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <!--[if lt IE 9]>
+   <script src="assets/js/html5shiv.js"></script>
+  <![endif]-->
+ </body>
+</html>
