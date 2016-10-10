6558f5805903e54eb141bdac0b18d79c6e7cfdad
Michael Golebiewski <promichael01@gmail.com>
2016-10-09 22:55:14 -0700

    Version 1.0.0 RELEASE

---
+++ src/shift/portfolio.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Portfolio - Pretty</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/normalize.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/theme.css?<?php echo time(); ?>">
+  <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+ </head>
+ <body>
+  <div class="navigation-container">
+   <div class="navigation">
+    <div class="logo-container">
+     <a href="#" class="logo shift">Pretty</a>
+    </div>
+    <ul>
+     <li><a href="index.php">Home</a></li>
+     <li><a href="portfolio.php">Portfolio</a></li>
+    </ul>
+   </div>
+  </div>
+  <div class="content-container">
+   <div class="content">
+    <h1 class="title shift">Selected Works from 2016 - <?php echo date("Y");?></h1>
+    <div class="filter">
+     <p class="sh">Table Filter</p>
+     <div class="form-s">
+      <a href="#">Lorem Ipsum <span class="badge">21</span></a>
+      <a href="#">Lorem Ipsum <span class="badge">488</span></a>
+      <a href="#">Lorem Ipsum <span class="badge">76</span></a>
+      <a href="#">Lorem Ipsum <span class="badge">9</span></a>
+      <a href="#">Lorem Ipsum <span class="badge">7</span></a>
+      <a href="#">Lorem Ipsum <span class="badge">82</span></a>
+     </div>
+    </div>
+    <div class="table">
+     <h3>2 Years of Designing and Developing</h3>
+     <h2>3 + projects and counting!</h2>
+     <h1>3 Results</h1>
+     <div class="data">
+      <h1 class="dTitle p">2016</h1>
+      <div class="item">
+       <h1 class="iTitle">Lorem Ipsum</h1>
+       <div class="sRef">
+        <a href="#" data-tooltip="View Concept" data-placement="bottom"><i class="fa fa-paint-brush shift"></i></a>
+        <a href="#" data-tooltip="View Website" data-placement="bottom"><i class="fa fa-picture-o shift"></i></a>
+       </div>
+       <p class="iSub"><span class="sTag">Website</span> for Onyx</p>
+       <p class="desc">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </p>
+      </div>
+      <div class="item">
+       <h1 class="iTitle">Lorem Ipsum</h1>
+       <div class="sRef">
+        <a href="#" data-tooltip="View Concept" data-placement="bottom"><i class="fa fa-paint-brush shift"></i></a>
+        <a href="#" data-tooltip="View Website" data-placement="bottom"><i class="fa fa-picture-o shift"></i></a>
+       </div>
+       <p class="iSub"><span class="sTag">Website</span> for Onyx</p>
+       <p class="desc">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </p>
+      </div>
+     </div>
+     <div class="data">
+      <h1 class="dTitle p">2016</h1>
+      <div class="item">
+       <h1 class="iTitle">Lorem Ipsum</h1>
+       <div class="sRef">
+        <a href="#" data-tooltip="View Concept" data-placement="bottom"><i class="fa fa-paint-brush shift"></i></a>
+        <a href="#" data-tooltip="View Website" data-placement="bottom"><i class="fa fa-picture-o shift"></i></a>
+       </div>
+       <p class="iSub"><span class="sTag">Website</span> for Onyx</p>
+       <p class="desc">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </p>
+      </div>
+      <div class="item">
+       <h1 class="iTitle">Lorem Ipsum</h1>
+       <div class="sRef">
+        <a href="#" data-tooltip="View Concept" data-placement="bottom"><i class="fa fa-paint-brush shift"></i></a>
+        <a href="#" data-tooltip="View Website" data-placement="bottom"><i class="fa fa-picture-o shift"></i></a>
+       </div>
+       <p class="iSub"><span class="sTag">Website</span> for Onyx</p>
+       <p class="desc">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </p>
+      </div>
+     </div>
+     <div class="data">
+      <h1 class="dTitle p">2016</h1>
+      <div class="item">
+       <h1 class="iTitle">Lorem Ipsum</h1>
+       <div class="sRef">
+        <a href="#" data-tooltip="View Concept" data-placement="bottom"><i class="fa fa-paint-brush shift"></i></a>
+        <a href="#" data-tooltip="View Website" data-placement="bottom"><i class="fa fa-picture-o shift"></i></a>
+       </div>
+       <p class="iSub"><span class="sTag">Website</span> for Onyx</p>
+       <p class="desc">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </p>
+      </div>
+      <div class="item last">
+       <h1 class="iTitle">Lorem Ipsum</h1>
+       <div class="sRef">
+        <a href="#" data-tooltip="View Concept" data-placement="bottom"><i class="fa fa-paint-brush shift"></i></a>
+        <a href="#" data-tooltip="View Website" data-placement="bottom"><i class="fa fa-picture-o shift"></i></a>
+       </div>
+       <p class="iSub"><span class="sTag">Website</span> for Onyx</p>
+       <p class="desc">Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum </p>
+      </div>
+     </div>
+    </div>
+   </div>
+  </div>
+  <div class="footer-container">
+   <div class="footer">
+    <div class="social">
+     <a href="#"><i class="fa fa-github shift" aria-hidden="true"></i></a>
+     <a href="#"><i class="fa fa-linkedin-square shift" aria-hidden="true"></i></a>
+     <a href="#"><i class="fa fa-twitter shift" aria-hidden="true"></i></a>
+     <a href="#"><i class="fa fa-paper-plane shift" aria-hidden="true"></i></a>
+    </div>
+    <div class="copy">
+     <p><i class="fa fa-copyright" aria-hidden="true"></i> <?php echo date("Y"); ?> <a href="#" class="shift">Pretty</a> All Rights Reserved</p>
+    </div>
+   </div>
+  </div>
+  <!-- js -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <!--[if lt IE 9]>
+   <script src="assets/js/html5shiv.js"></script>
+  <![endif]-->
+ </body>
+</html>
